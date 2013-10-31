afc7d8a2bdd34e9a516913803a5f917fe7361dc7
Eric Peden <eric@ericpeden.com>
2013-10-31 11:11:32 -0700

    Initial code/content commit.

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Eric Peden'
+SITENAME = u'I Get You More Juice'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('/r/ecig', '...'),
+          ('Tasty Vapor', 'http://tastyvapor.us/'),
+          ('Wizard Labs', ''),
+          ('RTS Vapes', ''),
+          ('Pelican', 'http://getpelican.com/'),
+	 )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 25
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
